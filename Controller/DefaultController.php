9a3787ff74e47546bb2b47d33e29374df7d9c33d
Michael Pohl <michael@runalyze.com>
2017-03-19 10:11:20 +0100

    controller annotations

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,15 +1,23 @@
 <?php
+
 namespace Runalyze\Bundle\GlossaryBundle\Controller;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+
 /**
  * Class DefaultController
  * @package Runalyze\Bundle\GlossaryBundle\Controller
  */
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/", name="glossary_index")
+     * @return Response
+     */
     public function indexAction()
     {
         return $this->render('GlossaryBundle::overview.html.twig');
--- GlossaryBundle.php
+++ GlossaryBundle.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace Runalyze\Bundle\GlossaryBundle;
+
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+
 class GlossaryBundle extends Bundle
 {
 }
