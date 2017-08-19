b292a32e307a7581d0f4e9bab04470ed9a8c13fe
Michael Pohl <michael@runalyze.com>
2017-08-19 16:50:36 +0200

    make glossary available for external viewers

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,161 +5,177 @@ namespace Runalyze\Bundle\GlossaryBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
+    private function renderCorrectView($view, $request) {
+
+        if ($request->isXmlHttpRequest()) {
+            return $this->render($view, [
+                'extends' => 'base_logged_in.twig',
+                'urlClass' => ''
+            ]);
+        } else {
+            return $this->render($view, [
+                'extends' => 'base_not_logged_in.html.twig',
+                'urlClass' => 'class="external"'
+            ]);
+        }
+    }
+
     /**
      * @Route("/", name="glossary-index")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('GlossaryBundle::overview.html.twig');
+        return $this->renderCorrectView('GlossaryBundle::overview.html.twig', $request);
     }
 
     /**
      * @Route("/atl", name="glossary-atl")
      */
-    public function atlAction()
+    public function atlAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:atl.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:atl.html.twig', $request);
     }
 
     /**
      * @Route("/ctl", name="glossary-ctl")
      */
-    public function ctlAction()
+    public function ctlAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:ctl.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:ctl.html.twig', $request);
     }
 
     /**
      * @Route("/tsb", name="glossary-tsb")
      */
-    public function tsbAction()
+    public function tsbAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:tsb.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:tsb.html.twig', $request);
     }
 
     /**
      * @Route("/easy-trimp", name="glossary-easy-trimp")
      */
-    public function easyTrimpAction()
+    public function easyTrimpAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:easy_trimp.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:easy_trimp.html.twig', $request);
     }
 
     /**
      * @Route("/heat-index", name="glossary-heat-index")
      */
-    public function heatIndexAction()
+    public function heatIndexAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:heat_index.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:heat_index.html.twig', $request);
     }
 
     /**
      * @Route("/hrv", name="glossary-hrv")
      */
-    public function hrvAction()
+    public function hrvAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:hrv.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:hrv.html.twig', $request);
     }
 
     /**
      * @Route("/marathon-shape", name="glossary-marathon-shape")
      */
-    public function marathonShapeAction()
+    public function marathonShapeAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:marathon_shape.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:marathon_shape.html.twig', $request);
     }
 
     /**
      * @Route("/maximum-heart-rate", name="glossary-maximum-heart-rate")
      */
-    public function maximumHeartRateAction()
+    public function maximumHeartRateAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:maximum_heart_rate.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:maximum_heart_rate.html.twig', $request);
     }
 
     /**
      * @Route("/monotony", name="glossary-monotony")
      */
-    public function monotonyAction()
+    public function monotonyAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:monotony.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:monotony.html.twig', $request);
     }
 
     /**
      * @Route("/performance-condition", name="glossary-performance-condition")
      */
-    public function performanceConditionAction()
+    public function performanceConditionAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:performance_condition.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:performance_condition.html.twig', $request);
     }
 
     /**
      * @Route("/resting-heart-rate", name="glossary-resting-heart-rate")
      */
-    public function restingHeartRateAction()
+    public function restingHeartRateAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:resting_heart_rate.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:resting_heart_rate.html.twig', $request);
     }
 
     /**
      * @Route("/rpe", name="glossary-rpe")
      */
-    public function rpeAction()
+    public function rpeAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:rpe.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:rpe.html.twig', $request);
     }
 
     /**
      * @Route("/swolf", name="glossary-swolf")
      */
-    public function swolfAction()
+    public function swolfAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:swolf.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:swolf.html.twig', $request);
     }
 
     /**
      * @Route("/training-effect", name="glossary-training-effect")
      */
-    public function trainingEffectAction()
+    public function trainingEffectAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:training_effect.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:training_effect.html.twig', $request);
     }
 
     /**
      * @Route("/training-strain", name="glossary-training-strain")
      */
-    public function trainingStrainAction()
+    public function trainingStrainAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:training_strain.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:training_strain.html.twig', $request);
     }
 
     /**
      * @Route("/trimp", name="glossary-trimp")
      */
-    public function trimpAction()
+    public function trimpAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:trimp.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:trimp.html.twig', $request);
     }
 
     /**
      * @Route("/vo2max", name="glossary-vo2max")
      */
-    public function vo2maxAction()
+    public function vo2maxAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:vo2max.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:vo2max.html.twig', $request);
     }
 
     /**
      * @Route("/windchill", name="glossary-windchill")
      */
-    public function windchillAction()
+    public function windchillAction(Request $request)
     {
-        return $this->render('GlossaryBundle:topics:windchill.html.twig');
+        return $this->renderCorrectView('GlossaryBundle:topics:windchill.html.twig', $request);
     }
 
     /**
